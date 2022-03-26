631245be4a5dbe5e98e7bedf2125ac56cfa2782e
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2022-03-26 17:06:22 -0300

    feat: add allwed positions in string

--- src/domain/table/table.ts
+++ src/domain/table/table.ts
@@ -1,5 +1,7 @@
 import { ForeignPositionError } from "./tableErros";
 
+export const allowedPositions = ["0", "1", "2", "3", "4"];
+
 export class Table {
   validateRobotPosition(position: number[]): boolean {
     const isForeignPosition = position.some((index) => {
