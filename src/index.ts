6645e58ebb891f6288f640952fc491e2de52aefc
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2022-03-28 19:27:02 -0300

    add server application

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+import express from "express";
+import { routes } from "./routes";
 
+const app = express();
+const port = 8080;
+app.use(routes);
+const server = app.listen(port, () => {
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  console.log(`listening on port ${port} 🚀`);
+});
+
+export { server };
