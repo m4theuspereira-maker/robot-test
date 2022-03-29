d0cec862435cc598c266c8e3a75530645afde472
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2022-03-29 09:18:58 -0300

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { routes } from "./routes";
-
+import cors from "cors";
 const app = express();
 const port = 8080;
+app.use(cors());
 app.use(routes);
 const server = app.listen(port, () => {
   app.use(express.json());
