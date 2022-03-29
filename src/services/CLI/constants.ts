e16557676bc43cac1dd700ce25432b6da08f078a
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2022-03-29 19:00:20 -0300

    move inquirer config

---
+++ src/services/CLI/constants.ts
@@ -0,0 +1,14 @@
+export const promptConfig = [
+  {
+    type: "input",
+    name: "position",
+    message: "Place the robot",
+    default: "place 0,0 north"
+  },
+  {
+    type: "input",
+    name: "moviment",
+    message: "Move robot to right, left, ahead, report the current position ",
+    default: "move"
+  }
+];
